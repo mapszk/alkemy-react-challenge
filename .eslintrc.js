1361ac01cbdcfb01e3817613c062a76761762b43
Martin Pilaszek <martinpilaszek@gmail.com>
2021-08-31 13:17:36 -0300

    Proyecto iniciado, instaladas dependecias y configurado el linter

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react-in-jsx-scope": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComa: "es5",
+        tabWidth: 2,
+        semi: false,
+        singleQuote: false,
+        endOfLine: "auto",
+      },
+    ],
+  },
+}
